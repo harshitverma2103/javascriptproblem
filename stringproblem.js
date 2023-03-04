0da6371b8fd2d41547b632429a1c144feeb696f6
harshitverma2103 <harshitverma2103@gmail.com>
2023-03-04 21:48:14 +0530

    finding out how many times the first char repeated

--- stringproblem.js
+++ stringproblem.js
@@ -3,7 +3,7 @@
 const find_Digits = (str) => {
     let digits = "";
     for (let i = 0; i <= str.length; i++) {
-        if(str[i] >= '0' && str[i] <= '9') {
+        if (str[i] >= '0' && str[i] <= '9') {
             digits += str[i];
         }
     }
@@ -12,3 +12,19 @@ const find_Digits = (str) => {
 
 console.log(find_Digits("hello123"));
 console.log(find_Digits("12harsh08"));
+
+//problem 2
+
+const repeatable_Char = (str) => {
+    let result = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[0] == str[i]) {
+            result++
+        }
+    }
+    return result;
+}
+
+console.log(repeatable_Char("testable"));
+console.log(repeatable_Char("octopus"));
+console.log(repeatable_Char("repeat"));
