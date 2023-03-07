b13e0d2254d3f640950f045cbcc935f29566e4a5
harshitverma2103 <harshitverma2103@gmail.com>
2023-03-07 13:51:23 +0530

    checking palindrome

--- stringproblem.js
+++ stringproblem.js
@@ -174,3 +174,16 @@ const reverse = (str) => {
 }
 
 console.log(reverse("happy"));
+
+
+// problem 11 
+
+const isPalindrome = (str) => {
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] != str[str.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(isPalindrome('deed'));
