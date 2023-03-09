0f1b0542c8b0f94bcf38b048fa791e10dc5b68e0
harshitverma2103 <harshitverma2103@gmail.com>
2023-03-09 12:26:02 +0530

    use join method to add underscore in array

--- arraymethods.js
+++ arraymethods.js
@@ -3,3 +3,10 @@
 let array = [1, 3, 6];
 let changeToString = array.toString();
 console.log(changeToString);
+
+
+// 2 . join 
+
+let arr = [12, 15, 13, 87] ;
+let newArr =  arr.join("-") ;
+console.log(newArr);
