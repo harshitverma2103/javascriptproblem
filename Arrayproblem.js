ecf0a649463a120ffb538e37d52c99634e80c1c7
harshitverma2103 <harshitverma2103@gmail.com>
2023-03-13 16:39:52 +0530

    finding even number from an array

--- Arrayproblem.js
+++ Arrayproblem.js
@@ -14,5 +14,18 @@ console.log(findOddNum([1, 2, 3, 4]));
 console.log(findOddNum([11, 24, 37, 43, 98, 31]));
 console.log(findOddNum([41, 52, 83, 74, 88]));
 
+// problem 2 
 
+const findEvenNum = (Array) => {
+  let oddNumbers = [];
+  for (let i = 0; i < Array.length; i++) {
+    if (Array[i] % 2 == 0) {
+      oddNumbers.push(Array[i])
+    }
+  }
+  return oddNumbers
+}
 
+console.log(findEvenNum([1, 2, 3, 4]));
+console.log(findEvenNum([11, 24, 37, 43, 98, 31]));
+console.log(findEvenNum([41, 52, 83, 74, 88]));
